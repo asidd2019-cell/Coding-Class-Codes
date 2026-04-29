de62ac5516e4fcbbd98482bc93b2d91e707082df
asidd2019-cell <asidd2019@gmail.com>
2026-04-29 19:01:03 -0400

    Youtube Clone Part 9

--- Web Development/Youtube Clone/javascript.js
+++ Web Development/Youtube Clone/javascript.js
@@ -1,5 +1,14 @@
-const searchBarInput = document.getElementById("searchBarInput")
+// const searchBarInput = document.getElementById("searchBarInput")
 
-searchBarInput.addEventListener("focus",function (){
+// searchBarInput.addEventListener("focus",function (){
 
-})
+// })
+
+const topicsParent = document.getElementById("topicsParent")
+const rightBtn = document.getElementById("rightTopicButton")
+
+const amountOfScroll = 800
+
+rightBtn.addEventListener('click', function () {
+    topicsParent.scrollBy({left:amountOfScroll,behavior:"smooth"})
+});
