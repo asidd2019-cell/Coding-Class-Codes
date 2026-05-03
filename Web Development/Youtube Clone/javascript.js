3b444fd05392a9a31da68748e6baa0e38bec20e0
asidd2019-cell <asidd2019@gmail.com>
2026-05-03 18:56:31 -0400

    Youtube Clone Part 10

--- Web Development/Youtube Clone/javascript.js
+++ Web Development/Youtube Clone/javascript.js
@@ -6,9 +6,40 @@
 
 const topicsParent = document.getElementById("topicsParent")
 const rightBtn = document.getElementById("rightTopicButton")
+const leftBtn = document.getElementById("leftTopicButton")
+
 
 const amountOfScroll = 800
 
 rightBtn.addEventListener('click', function () {
     topicsParent.scrollBy({left:amountOfScroll,behavior:"smooth"})
 });
+
+leftBtn.addEventListener('click', function () {
+    console.log("not working");
+    topicsParent.scrollBy({ left: -amountOfScroll, behavior: "smooth" });
+});
+
+function updateScrollButtonsDisplay () {
+    if (topicsParent.scrollLeft <= 0) {
+        leftBtn.style.display = "none"
+    }
+
+    else {
+        leftBtn.style.display = "flex"
+    }
+
+    const maxScroll = topicsParent.scrollWidth - topicsParent.clientWidth;
+    if (topicsParent.scrollLeft >= maxScroll) {
+        rightBtn.style.display = "none"
+    }
+
+    else {
+        rightBtn.style.display = "flex"
+    }
+}
+
+
+topicsParent.addEventListener('scroll',updateScrollButtonsDisplay )
+
+updateScrollButtonsDisplay()
