4591556dad71e01fa43750d1da07829a277c37f0
asidd2019-cell <asidd2019@gmail.com>
2026-06-07 18:45:04 -0400

    To-Do List Project Completed

--- Web Development/All about Javascript/To-Do List/script.js
+++ Web Development/All about Javascript/To-Do List/script.js
@@ -2,6 +2,10 @@ onPageLoad()
 var tasks = []
 var current_task_id = 1
 const form = document.getElementById("form")
+let currentUpdateId = null;
+const modal = document.getElementById("updateModal");
+const saveButton = document.getElementById("saveUpdate");
+const closeButton = document.getElementById("closeModal");
 
 form.addEventListener("submit",function (e){
     e.preventDefault()
@@ -51,6 +55,7 @@ function addTask(id,name,description,stDate,endDate){
     <td><button class="update-button">Update</button></td>
     <td><button class="delete-button">Delete</button></td>
     `
+    tr.id = id;
 
       const deleteButton = tr.querySelector(".delete-button");
 
@@ -62,6 +67,46 @@ function addTask(id,name,description,stDate,endDate){
 
         tr.remove();
     });
+
+    const updateButton = tr.querySelector(".update-button");
+
+    updateButton.addEventListener("click", function () {
+
+    const task = tasks.find(t => t.id === id);
+
+    currentUpdateId = id;
+
+    document.getElementById("updateName").value = task.name;
+    document.getElementById("updateDescription").value = task.description;
+    document.getElementById("updateStart").value = task.startDate;
+    document.getElementById("updateEnd").value = task.endDate;
+
+    modal.style.display = "flex";
+    });
+
+        closeButton.addEventListener("click", function () {
+        modal.style.display = "none";
+        });
+
+        saveButton.addEventListener("click", function () {
+
+    const task = tasks.find(t => t.id === currentUpdateId);
+
+    task.name = document.getElementById("updateName").value;
+    task.description = document.getElementById("updateDescription").value;
+    task.startDate = document.getElementById("updateStart").value;
+    task.endDate = document.getElementById("updateEnd").value;
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    const tr = document.getElementById(currentUpdateId);
+
+    tr.children[0].textContent = task.name;
+    tr.children[1].textContent = task.description;
+    tr.children[2].textContent = task.startDate;
+    tr.children[3].textContent = task.endDate;
+
+    modal.style.display = "none";
+});
 
     const table = document.getElementById("table")
 
