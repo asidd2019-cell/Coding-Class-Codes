fdb850284b392d2cd0d257f4e2db9d854ff1c665
asidd2019-cell <asidd2019@gmail.com>
2026-06-08 18:54:57 -0400

    Started Arrays in Javascript

---
+++ Web Development/All about Javascript/Arrays In Javascript/script.js
@@ -0,0 +1,51 @@
+/*
+create a list of 5 number
+console log the total number of elements in it
+console log the third element 
+change the 4th value to something else
+Insert a new value at end
+remove a value from the end
+Loop over the entire list
+Loop over the entire list by skip count 2
+
+*/
+
+
+// TASK 1 
+let list = [1,2,3,4,5]
+//
+
+// TASK 2
+//console.log(list.length)
+//
+
+// TASK 3
+//console.log(list[2])
+//
+
+// TASK 4
+//list[3] = 10
+//console.log(list)
+//
+
+// TASK 5
+// list.push(6)
+// console.log(list)
+//
+
+// TASK 6
+// list.pop()
+// console.log(list)
+//
+
+// TASK 7
+// for (const i of list){
+//     console.log(list)
+// }
+//
+
+// TASK 8
+for (let i= 0;i<list.length; i=i+2){
+    console.log(list[i])
+}
+//
