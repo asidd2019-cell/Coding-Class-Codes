b804be1cd467bcfea0a830d2e720e6fbf1ac85e6
asidd2019-cell <asidd2019@gmail.com>
2026-06-09 19:53:38 -0400

    Learned lists in Javascript

--- Web Development/All about Javascript/Arrays In Javascript/script.js
+++ Web Development/All about Javascript/Arrays In Javascript/script.js
@@ -12,7 +12,7 @@ Loop over the entire list by skip count 2
 
 
 // TASK 1 
-let list = [1,2,3,4,5]
+// let list = [1,2,3,4,5]
 //
 
 // TASK 2
@@ -45,7 +45,62 @@ let list = [1,2,3,4,5]
 //
 
 // TASK 8
-for (let i= 0;i<list.length; i=i+2){
-    console.log(list[i])
-}
+// for (let i= 0;i<list.length; i=i+2){
+//     console.log(list[i])
+// }
 //
+
+// let list = [1,2,3,4,5,6,7,8,9,10]
+// let new_list = list.filter(x => x % 2 == 0)
+// console.log (new_list)
+
+// let list = ["iqra","azam","nazia","ahsan"]
+// let new_list = list.filter(x => x.length > 4)
+// console.log(new_list)
+
+// let list = [2,17,19,5,7,18]
+// let new_list = list.filter(x => x > 10)
+// console.log(new_list)
+
+// let list = ["adfw","bedfvgsdf","azaxcdeafd","griuhgn" "pgjjufhg","asj"]
+// let new_list = list.filter(x => x.startsWith("a"))
+// console.log(new_list)
+
+// let list = [1,-5,7,2,-3,-9,6,0]
+// let new_list = list.filter(x => x >= 0)
+// console.log(new_list)
+
+// let list = [
+//     {
+//         "id" : 1,
+//         "name" : "Ahsan",
+//         "grade" : 7
+//     }
+//     ,
+//     {
+//         "id" : 2,
+//         "name" : "Abubakar",
+//         "grade" : 12
+//     }
+// ]
+// let new_list = list.filter(x => x.name == "Ahsan")
+// console.log(new_list)
+
+// const students = [
+//   { name: "Ali", score: 80 },
+//   { name: "Sara", score: 45 },
+//   { name: "Ahmed", score: 60 },
+//   { name: "Zara", score: 30 }
+// ];
+// let new_list = students.filter(x => x.score >= 50)
+// console.log(new_list)
+
+const items = [
+  { name: "Laptop", available: true },
+  { name: "Mouse", available: false },
+  { name: "Keyboard", available: true },
+  { name: "Monitor", available: false }
+];
+
+let new_list = items.filter(x => x.available == true)
+console.log(new_list)
