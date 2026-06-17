84a1657fd467c07f63f132654179ab381e43e1fd
asidd2019-cell <asidd2019@gmail.com>
2026-06-17 19:52:16 -0400

    Digital Clock Project completed

--- Web Development/All about Javascript/Digital Clock Project/script.js
+++ Web Development/All about Javascript/Digital Clock Project/script.js
@@ -2,6 +2,7 @@ let is24HourMode = true
 const hoursSpan = document.getElementById("hours")
 const minutesSpan = document.getElementById("minutes")
 const secondsSpan = document.getElementById("seconds")
+const AMPM = document.getElementById("amPm")
 
 document.getElementById("toggle_btn").addEventListener('click', function(){
     is24HourMode = !is24HourMode
@@ -15,10 +16,26 @@ function updateClock(){
     let seconds = currentTime.getSeconds()
 
     if (is24HourMode == true){
-        hoursSpan.textContent = hours
-        minutesSpan.textContent = minutes
-        secondsSpan.textContent = seconds
+        hoursSpan.textContent = String(hours).padStart(2, '0');
+        minutesSpan.textContent = String(minutes).padStart(2, '0');
+        secondsSpan.textContent = String(seconds).padStart(2, '0');
+        AMPM.style.display = "none"
     }
+    else{
+        AMPM.style.display = "flex"
+        if (hours < 12) {
+            AMPM.textContent = "AM";
+        }
+        else{
+            AMPM.textContent = "PM";
+        }
+        hours = hours % 12
+        hoursSpan.textContent = String(hours).padStart(2, '0');
+        minutesSpan.textContent = String(minutes).padStart(2, '0');
+        secondsSpan.textContent = String(seconds).padStart(2, '0');
+    }
+
+
 }
 
 updateClock()
