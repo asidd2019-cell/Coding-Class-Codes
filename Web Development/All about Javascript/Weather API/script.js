9846eb5c5d4bd3dfa0da385c399548fd8aebdedf
asidd2019-cell <asidd2019@gmail.com>
2026-07-12 19:50:17 -0400

    Weather API Completed

--- Web Development/All about Javascript/Weather API/script.js
+++ Web Development/All about Javascript/Weather API/script.js
@@ -3,11 +3,16 @@ const findWeatherButton = document.getElementById("button")
 const cityCountrySpan = document.getElementById("cityName")
 const tempCelSpan = document.getElementById("temp_cels_number")
 const tempFahrSpan = document.getElementById("temp_fahr_number")
+const tempCelFeelsLikeSpan = document.getElementById("temp_feels_like_cels_number")
+const tempFahrFeelsLikeSpan = document.getElementById("temp_feels_like_fahr_number")
 const windSpeedSpan = document.getElementById("wind_speed_number")
+const values = document.getElementById("values")
 
 findWeatherButton.addEventListener('click',async ()=>{
+    values.style.display = "flex"
+    values.style.flexDirection = "column"
     let city = citySelect.value
-    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=0f9f65342419461e869235451260607&q=east%20brunswick`
+    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=0f9f65342419461e869235451260607&q=${city}`
 
     let response = await fetch(apiUrl)
 
@@ -20,10 +25,14 @@ findWeatherButton.addEventListener('click',async ()=>{
     let country = data.location.country
     let temp_c = data.current.temp_c
     let temp_f = data.current.temp_f
+    let temp_c_feels_like = data.current.feelslike_c
+    let temp_f_feels_like = data.current.feelslike_f
     let wind_kph = data.current.wind_kph
 
     cityCountrySpan.innerHTML = `${city}, ${country}`
     tempCelSpan.innerHTML = `${temp_c}`
     tempFahrSpan.innerHTML = `${temp_f}`
+    tempCelFeelsLikeSpan.innerHTML = `${temp_c_feels_like}`
+    tempFahrFeelsLikeSpan.innerHTML = `${temp_f_feels_like}`
     windSpeedSpan.innerHTML = `${wind_kph}`
 })
