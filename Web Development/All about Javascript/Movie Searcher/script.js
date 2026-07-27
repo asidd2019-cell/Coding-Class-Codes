7f7e98eb337c18c68c440a77a485a7e2d11383b2
asidd2019-cell <asidd2019@gmail.com>
2026-07-27 19:50:56 -0400

    Movie Searcher Part 2

---
+++ Web Development/All about Javascript/Movie Searcher/script.js
@@ -0,0 +1,24 @@
+const search = document.getElementById("search")
+const searchBtn = document.getElementById("searchBtn")
+const movieImg = document.getElementById("movieImage")
+const movieTitle = document.getElementById("movieTitle")
+const releaseDate = document.getElementById("releaseDate")
+
+searchBtn.addEventListener('click',async ()=>{
+    let apiURL = `https://www.omdbapi.com/?apikey=c550fb3a&s=${search.value}&type=movie`
+    let response = await fetch(apiURL)
+
+    if (!response.ok){
+        alert("API Failed")
+    }
+
+    let data = await response.json()
+
+    let title = search.value
+    let release = data.Search[0].Year
+    let img = data.Search[0].Poster
+
+    movieTitle.innerHTML = `Title: ${title}`
+    releaseDate.innerHTML = `Release Date: ${release}`
+    movieImg.setAttribute("src",img)
+})
