3fa10c16c4dd2dcec9ad477d051002ca30a4eeae
asidd2019-cell <asidd2019@gmail.com>
2026-08-03 19:21:05 -0400

    Movie Searcher Part 7

--- Web Development/All about Javascript/Movie Searcher/script.js
+++ Web Development/All about Javascript/Movie Searcher/script.js
@@ -3,6 +3,8 @@ const searchBtn = document.getElementById("searchBtn")
 const movieImg = document.getElementById("movieImage")
 const movieTitle = document.getElementById("movieTitle")
 const releaseDate = document.getElementById("releaseDate")
+const searchedMoviesDiv = document.getElementById("searched_movies")
+
 
 searchBtn.addEventListener('click',searchMovie)
 
@@ -24,9 +26,39 @@ async function searchMovie(){
     if (data.Response == 'False'){
         alert("Movie not Found")
     }
-    let title = data.Search[0].Title
-    let release = data.Search[0].Year
-    let img = data.Search[0].Poster
+
+    searchedMoviesDiv.innerHTML = ''
+    for (let i=0; i<data.Search.length;i++){
+        const movieInfoDiv = document.createElement('div')
+        movieInfoDiv.classList.add('movie-info')
+        
+        const imgTag = document.createElement('img')
+        let img = data.Search[i].Poster
+        imgTag.setAttribute("src",img)
+
+        const movieDetailsDiv = document.createElement('div')
+        movieDetailsDiv.classList.add('movie-details')
+        
+        const titleSpan = document.createElement('span')
+        titleSpan.classList.add('movie-info-span')
+
+        const releaseDateSpan = document.createElement('span')
+        releaseDateSpan.classList.add('movie-info-span')
+
+        let title = data.Search[0].Title
+        let release = data.Search[0].Year
+
+        titleSpan.innerHTML = `Title: ${title}`
+        releaseDateSpan.innerHTML = `Release Date: ${release}`
+
+        movieInfoDiv.appendChild(imgTag)
+        movieInfoDiv.appendChild(movieDetailsDiv)
+
+        movieDetailsDiv.appendChild(titleSpan)
+        movieDetailsDiv.appendChild(releaseDateSpan)
+
+        searchedMoviesDiv.appendChild(movieInfoDiv)
+    }
 
     movieTitle.innerHTML = `Title: ${title}`
     releaseDate.innerHTML = `Release Date: ${release}`
