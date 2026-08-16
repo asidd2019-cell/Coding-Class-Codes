23291e4a81ac249e3c0a76308cd8727a007dfd1e
asidd2019-cell <asidd2019@gmail.com>
2026-08-16 19:54:21 -0400

    Notes App Part 3

--- Web Development/All about Javascript/Notes App/script.js
+++ Web Development/All about Javascript/Notes App/script.js
@@ -6,5 +6,35 @@ addButton.addEventListener("click", ()=> {
     let notesText = textArea.value 
     if (notesText.trim() == ""){
         alert ("Write something to add a note.")
+        return
     }
+    let noteDiv = document.createElement('div')
+    noteDiv.classList.add('note')
+
+    let noteDetailsDiv = document.createElement('div')
+    noteDetailsDiv.classList.add('note-details')
+    noteDetailsDiv.innerHTML = `${notesText}`
+    noteDetailsDiv.style.whiteSpace = "pre-wrap"
+
+    let customizeButtonsDiv = document.createElement('div')
+    customizeButtonsDiv.classList.add('customize-buttons')
+
+    let editButton = document.createElement('button')
+    editButton.classList.add('edit-button')
+     editButton.innerHTML = 'Edit'
+
+    let deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete-button')
+     deleteButton.innerHTML = 'Delete'
+
+    customizeButtonsDiv.appendChild(editButton)
+    customizeButtonsDiv.appendChild(deleteButton)
+
+    noteDiv.appendChild(noteDetailsDiv)
+    noteDiv.appendChild(customizeButtonsDiv)
+
+    notes.appendChild(noteDiv)
+
+    textArea.value = ''
 })
+
