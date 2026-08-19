799219681d60908277a33abb5a6a87000b609850
asidd2019-cell <asidd2019@gmail.com>
2026-08-19 19:54:35 -0400

    Notes App Part 5

--- Web Development/All about Javascript/Notes App/script.js
+++ Web Development/All about Javascript/Notes App/script.js
@@ -1,6 +1,9 @@
 const textArea = document.getElementById("textarea")
 const addButton = document.getElementById("add_button")
 const notes = document.getElementById("notes")
+const body = document.getElementById("body")
+// const modal = document.getElementById('modal')
+// modal.close ()
 
 let notesIds = 1
 addButton.addEventListener("click", ()=> {
@@ -28,7 +31,29 @@ addButton.addEventListener("click", ()=> {
     editButton.innerHTML = 'Edit'
 
     editButton.addEventListener("click", ()=> {
-        
+        // modal.style.display = 'flex'
+        // modal.showModal()
+
+        let modalDialog = document.createElement('dialog')
+        modalDialog.classList.add('modal')
+
+        let editNote = document.createElement('textarea')
+        editNote.classList.add('edit-note')
+        editNote.setAttribute('placeholder', "Type your note here")
+
+        let saveButtonDiv = document.createElement('div')
+        saveButtonDiv.classList.add('save-button-div')
+
+        let saveButton = document.createElement('button')
+        saveButton.classList.add('save-button')
+        saveButton.innerHTML = 'Save'
+
+        modalDialog.appendChild(editNote)
+        modalDialog.appendChild(saveButtonDiv)
+        saveButtonDiv.appendChild(saveButton)
+        body.appendChild(modalDialog)
+        modalDialog.style.display = 'flex'
+        modalDialog.showModal()
     })
 
     let deleteButton = document.createElement('button')
