5d445bdf1bc5950f752f2502c9eb71df75a7c30d
asidd2019-cell <asidd2019@gmail.com>
2026-09-13 19:51:55 -0400

    Expense Tracker Part 11

--- Web Development/All about Javascript/Expense Tracker/script.js
+++ Web Development/All about Javascript/Expense Tracker/script.js
@@ -50,27 +50,73 @@ function CalculateTotalIncomeAndExpense () {
     expenseSpan.innerHTML = "$ " + `${totalExpenseSum}`
 }
 
-function ShowTransaction {
+function ShowTransaction () {
   transactionInfoSpan = document.createElement("transactionInfoSpan")
+  transactionInfoSpan.classList.add ("transaction-info-span")
 
   transactionHistory = document.createElement("transactionHistory")
+  transactionHistory.classList.add ("transaction-history")
 
   incomeTransaction = document.createElement("incomeTransaction")
-  expenseTransaction = document.createElement("expenseTransaction")
+  incomeTransaction.classList.add ("income-transaction")
+  incomeTransaction.AppendChild (incomeTransactionDetails)
+  incomeTransaction.AppendChild (incomeTransactionDescription)
+  incomeTransaction.AppendChild (incomeTransactionDate)
+  incomeTransaction.AppendChild (incomeAmountAndCancel)
 
   incomeTransactionDetails = document.createElement("incomeTransactionDetails")
+  incomeTransactionDetails.classList.add ("transaction-details")
 
   incomeTransactionDescription = document.createElement("incomeTransactionDescription")
+  incomeTransactionDescription.classList.add ("transaction-description")
 
   incomeTransactionDate = document.createElement("incomeTransactionDate")
+  incomeTransactionDate.classList.add ("transaction-date")
 
-  amountAndCancel = document.createElement("amountAndCancel")
-  amountAndCancel.AppendChild (incomeTransactionAmount)
-  amountAndCancel.AppendChild (incomeTransactionAmountSpan)
+  incomeAmountAndCancel = document.createElement("incomeAmountAndCancel")
+  incomeAmountAndCancel.classList.add ("amount-and-cancel")
+  incomeAmountAndCancel.AppendChild (incomeTransactionAmount)
+  incomeAmountAndCancel.AppendChild (incomeTransactionAmountSpan)
 
   incomeTransactionAmount = document.createElement("incomeTransactionAmount")
+  incomeTransactionAmount.classList.add ("income-transaction-amount")
+  incomeTransactionAmount.AppendChild (incomeTransactionAmountSpan)
 
   incomeTransactionAmountSpan = document.createElement("incomeTransactionAmountSpan")
 
-  cancelTransaction = document.createElement("cancelTransaction")
+  incomeCancelTransaction = document.createElement("incomeCancelTransaction")
+  incomeCancelTransaction.classList.add ("fa-solid")
+  incomeCancelTransaction.classList.add ("fa-x")
+//  _____________________________________________________________________________________________
+
+  expenseTransaction = document.createElement("expenseTransaction")
+  expenseTransaction.classList.add ("expense-transaction")
+  expenseTransaction.AppendChild (expenseTransactionDetails)
+  expenseTransaction.AppendChild (expenseTransactionDescription)
+  expenseTransaction.AppendChild (expenseTransactionDate)
+  expenseTransaction.AppendChild (expenseAmountAndCancel)
+
+  expenseTransactionDetails = document.createElement("expenseTransactionDetails")
+  expenseTransactionDetails.classList.add ("transaction-details")
+
+  expenseTransactionDescription = document.createElement("expenseTransactionDescription")
+  expenseTransactionDescription.classList.add ("transaction-description")
+
+  expenseTransactionDate = document.createElement("expenseTransactionDate")
+  expenseTransactionDate.classList.add ("transaction-date")
+
+  expenseAmountAndCancel = document.createElement("expenseAmountAndCancel")
+  expenseAmountAndCancel.classList.add ("amount-and-cancel")
+  expenseAmountAndCancel.AppendChild (expenseTransactionAmount)
+  expenseAmountAndCancel.AppendChild (expenseTransactionAmountSpan)
+
+  expenseTransactionAmount = document.createElement("expenseTransactionAmount")
+  expenseTransactionAmount.classList.add ("expense-transaction-amount")
+  expenseTransactionAmount.AppendChild (expenseTransactionAmountSpan)
+
+  expenseTransactionAmountSpan = document.createElement("expenseTransactionAmountSpan")
+
+  expenseCancelTransaction = document.createElement("expenseCancelTransaction")
+  expenseCancelTransaction.classList.add ("fa-solid")
+  expenseCancelTransaction.classList.add ("fa-x")
 }
